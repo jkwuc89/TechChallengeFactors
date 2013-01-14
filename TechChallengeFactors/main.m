0343f9604ab9a61ac5f1eaae79af3f7554d38b01
Keith Wedinger <jkwuc89@gmail.com>
2013-01-13 19:29:03 -0500

    Initial Commit

---
+++ TechChallengeFactors/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TechChallengeFactors
+//
+//  Created by Keith Wedinger on 1/13/13.
+//  Copyright (c) 2013 Keith Wedinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
