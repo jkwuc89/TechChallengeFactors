9e60d08a28aa41938e68ec3430c190a7092d70bb
Keith Wedinger <jkwuc89@gmail.com>
2013-01-13 20:26:43 -0500

    Input file read into sorted array

--- TechChallengeFactors/main.m
+++ TechChallengeFactors/main.m
@@ -8,13 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+//
+// Display syntax for this program
+//
+void displaySyntax() {
+    NSLog( @"Syntax: TechChallengeFactors <factors.txt>" );
+}
 
+//
+// Main entry point
+//
+int main(int argc, const char * argv[]) {
+    
+    // Use Cocoa's reference counting
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Must have 2 command line parms, 1 being the command itself
+        if ( argc != 2 ) {
+            displaySyntax();
+            return 1;
+        }
+        
+        // 2nd command line parm must be a valid file
+        NSFileManager *fileMgr = [NSFileManager defaultManager];
+        NSString *inputFilePath = [[NSString alloc] initWithCString:argv[1] encoding:NSASCIIStringEncoding];
+        if ( ![fileMgr fileExistsAtPath:inputFilePath] ) {
+            NSLog( @"*** ERROR *** %@ does not exist", inputFilePath );
+            return 1;
+        }
+        
+        // Read in date/time stamps from the input file
+        NSString *fileContents = [NSString stringWithContentsOfFile:inputFilePath encoding:NSASCIIStringEncoding error:NULL];
+        NSArray *dateTimeStamps = [[fileContents componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]]
+                                   sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        NSLog( @"%@ has %lu date/time stamps", inputFilePath, dateTimeStamps.count );
+        
         
     }
     return 0;
